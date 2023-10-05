152120f90fab5d5effe2527fdbd57b439bcfe0f5
yasabri <yasabri04@gmail.com>
2023-10-05 15:40:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 /**
-* main - main function
-* 
-* Return: Always 0 (Success)
-*/
+ * main - main function
+ * 
+ * Return: Always 0 
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
-
-
