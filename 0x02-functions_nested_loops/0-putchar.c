64badefee9090bbe72642a21065cc306e25ab4b6
yasabri <yasabri04@gmail.com>
2023-10-10 12:21:28 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
 /**
  * main - print _putchar
  * is written by mbah
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar("\n");
+
+	return (0);
 }
