ce95b6743ee942d1bb1977f6e42ec2e56a5a7095
yasabri <yasabri04@gmail.com>
2023-10-10 15:31:53 -0700

    alphabitx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints ten times the alphabet, in lowercase
+ */
+
+void print_alphabet ten times(void)
+{
+	char line, ch;
+
+	for (line = 0; line <= 10; line++)
+	{
+		for (ch = "a"; ch <= "z"; ch++)
+	
+			_putchar(ch);
+		_putchar('\n');
+
+	}
+}
