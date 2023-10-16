5164bba7d1f4a6ee8c49c0ef36c85e26d80b3ba3
yasabri <yasabri04@gmail.com>
2023-10-16 15:45:56 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _strlen - return the length of a string
+ * _strlen - return the length 
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
  */
 int _strlen(char *s)
 {
-	int x;
+        int count;
 
-	for (x = 0; *s != '\0' is++;)
-		x++;
+        for (count = 0; *s != '\0'; s++;)
+                count++;
 
-	return (x);
+        return (count);
 }
