b09b4457bf3db9f409db900dcc0e2132f4cc71b9
yasabri <yasabri04@gmail.com>
2023-10-16 16:05:05 -0700

    strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * Owned By Bwave
  * @s: char to check
  * Description: this will return the length of a string
- * Return: 0 is success
+ * Return: (0) success
  */
 int _strlen(char *s)
 {
-	int a = 0;
+	int x = 0;
 
 	for (; *s++;)
-		a++;
-	return (a);
+		x++;
+	return (x);
 }
