94f10858ed8e8b2eb7cd6d150815128a5e8005b2
yasabri <yasabri04@gmail.com>
2023-10-19 16:10:57 -0700

    reve

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
  * reverse_array - reverses the content of an array of integers
  * @a:array
  * @n:integer
- * Return:()
+ * Return: ()
  */
 void reverse_array(int *a, int n)
 {
@@ -15,4 +15,4 @@ for (i = 0; (i < (n - 1) / 2); i++)
 	a[i] = a[n - 1 - i];
 	a[n - 1 - i] = c;
 	}
-}	
+}
