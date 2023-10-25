50805bd7686920ea544b0be50e8ec33bd8af40cc
yasabri <yasabri04@gmail.com>
2023-10-25 16:46:37 -0700

    fact

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,19 +1,16 @@
 #include "main.h"
+
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
  *
- * Return: The length of the string.
+ * Return: factorial of n
  */
-int _strlen_recursion(char *s)
+int factorial(int n)
 {
-	int longit = 0;
-
-	if (*s)
-	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
-	}
-
-	return (longit);
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
