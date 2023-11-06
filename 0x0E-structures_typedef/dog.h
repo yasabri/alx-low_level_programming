e279b5fe56b34948a96cc10621ec23d5eb4552e5
yasabri <yasabri04@gmail.com>
2023-11-06 07:26:11 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: name member
+ * @age: age of member
+ * @owner: owner member
+ *
+ * Description: Longer description
+ */
+
+#include <stdio.h>
+struct dog{
+    char * = name
+    float = age
+    char * = owner
+};
+
+#endif
