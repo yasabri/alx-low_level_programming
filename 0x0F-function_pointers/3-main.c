00cf2725375f75747b019ca5576696c7276e2d08
yasabri <yasabri04@gmail.com>
2023-11-08 14:21:09 -0800

    m

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
+ */
 int main(int argc, char *argv[])
 {
 	int (*oprt)(int, int);
