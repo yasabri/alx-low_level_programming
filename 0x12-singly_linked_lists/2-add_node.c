61dad769929089b4e9cfbaef79f5955e97a646de
yasabri <yasabri04@gmail.com>
2023-11-16 14:55:10 -0800

    2,1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,26 +11,26 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
+	char *x;
+	int y;
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	dup = strdup(str);
-	if (dup == NULL)
+	x = strdup(str);
+	if (x == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
 
-	for (len = 0; str[len];)
-		len++;
+	for (y = 0; str[y];)
+		y++;
 
-	new->str = dup;
-	new->len = len;
+	new->str = x;
+	new->y = y;
 	new->next = *head;
 
 	*head = new;
