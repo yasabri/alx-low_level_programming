d1fd4587eee88a1609d3f2cc5655ef3a26d2682d
yasabri <yasabri04@gmail.com>
2023-11-20 12:53:14 -0800

    print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t x = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		x++;
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (x);
+	return (nodes);
 }
