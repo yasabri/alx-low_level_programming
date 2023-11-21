1a1de4e2ae2453b6bed70c931121bc54386d9426
yasabri <yasabri04@gmail.com>
2023-11-21 03:55:18 -0800

    add

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * *add_nodeint_end - adds a new node at the end of a linked list.
+ *
+ * @head: pointer to a pointer to head.
+ * @n: integr node
+ * Return: address of new elementor null if it failed.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *old, *new;
+
+	newnode = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		old = *head;
+		while (old->next != NULL)
+		{
+			old = old->next;
+		}
+		old->next = new;
+	}
+	return (new);
+}
