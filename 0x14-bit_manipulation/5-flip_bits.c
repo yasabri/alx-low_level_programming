8459c321d4ab3af942614a483e9f5cec4e1ddb1d
yasabri <yasabri04@gmail.com>
2023-11-23 12:10:11 -0800

    4,5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - quantity of bits you would 
+ * flip_bits - quantity of bits you would
  * have to flip in order to change a number.
  * @n: first number.
  * @m: second number.
