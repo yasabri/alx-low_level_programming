c42321bfab312332138b9644fce2de5af8fe3ae4
yasabri <yasabri04@gmail.com>
2023-11-27 11:33:21 -0800

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,48 +1,48 @@
 #include "main.h"
 /**
- * main - content of a file to another one
+ * main - transor copy content of a file to another one
  * @argc: argument count
  * @argv: argument vector
- * Return: 0 maenthat success
+ * Return: 0 if success
  */
 
 int main(int argc, char *argv[])
 {
-	int firstfile, secondfile, read, x, y;
+	int fileone, filetwo, _read, x, y;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-	firstfile = open(argv[1], O_RDONLY);
-	if (firstfile < 0)
+	fileone = open(argv[1], O_RDONLY);
+	if (fileone < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	secondfile = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((read = read(firstfile, buffer, 1024)) > 0)
+	filetwo = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((_read = read(fileone, buffer, 1024)) > 0)
 	{
-		if (secondfile < 0 || (write(secondfile, buffer, read) != read))
+		if (filetwo < 0 || (write(filetwo, buffer, _read) != _read))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
 	}
-	if (read < 0)
+	if (_read < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	c1 = close(firstfile);
+	x = close(fileone);
 	if (x < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", firstfile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fileone), exit(100);
 	}
-	c2 = close(secondfile);
+	y = close(filetwo);
 	if (y < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", secondfile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", filetwo), exit(100);
 	}
 	return (0);
 }
