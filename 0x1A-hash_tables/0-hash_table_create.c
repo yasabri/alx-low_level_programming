b9ce9e7ee79f1c09394328b29f9129bab1fa75ce
yasabri <yasabri04@gmail.com>
2024-02-02 11:11:13 -0800

    p

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,15 +1,16 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Create hash table.
- * @size: size of array
- * Return: If error occurs - NULL
- *         or - a pointer to the new hash table.
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int x;
+	unsigned long int i;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
@@ -19,8 +20,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-	for (x = 0; x < size; x++)
-		ht->array[x] = NULL;
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
