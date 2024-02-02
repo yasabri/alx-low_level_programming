eb1ac62d4273166add4ab768fbce3bf56165ba91
yasabri <yasabri04@gmail.com>
2024-02-02 10:51:43 -0800

    pu

---
+++ 0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create hash table.
+ * @size: size of array
+ * Return: If error occurs - NULL
+ *         or - a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int x;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (x = 0; x < size; x++)
+		ht->array[x] = NULL;
+
+	return (ht);
+}
